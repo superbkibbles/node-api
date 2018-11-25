e6db6732995d8fd321192c5a6da9c4703915126f
Mand <mend_mzury@hotmail.com>
2018-11-25 14:40:49 +0200

    updating

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require("lodash")
 const express = require("express")
 const bodyParser = require("body-parser")
 
@@ -61,6 +62,28 @@ app.delete("/todos/:id", (req, res)=>{
   }
   Todo.findByIdAndRemove(todoId).then((todo)=>{
     if (!todo) {
+      return res.status(404).send()
+    }
+    res.send({todo: todo})
+  }).catch((err)=>{
+    res.status(400).send()
+  })
+})
+
+app.patch("/todos/:id", (req, res)=>{
+  var todoId = req.params.id
+  var body = _.pick(req.body, ['text', 'completed']);
+  if (!ObjectID.isValid(todoId)) {
+    return res.status(404).send()
+  }
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime()
+  } else{
+    body.completedAt = null
+    body.completed = false
+  }
+  Todo.findByIdAndUpdate(todoId, {$set: body}, { new: true}).then((todo)=>{
+    if(!todo){
       return res.status(404).send()
     }
     res.send({todo: todo})
